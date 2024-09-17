11ae2d46ce6cb2f4037d3c031106483ed4686c64
anon-11 <114279690+anon-11@users.noreply.github.com>
2024-09-17 11:42:11 +0530

    Add files via upload

---
+++ dl.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plot
+
+#part b1:
+
+x=(-2*np.pi, 2*np.pi,1000)
+y=np.sin(x)
+
+    #splitting into 4 equal parts:
+split_x=np.split(x,4)
+split_y=np.split(y,4)
+
+#part b2:
+
+x_validation = np.random.uniform(-2*np.pi,2*np.pi,300)
+
