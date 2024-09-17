da7f0e6874b064c89528186a08919855bb64c47c
coder-bean <102741229+coder-bean@users.noreply.github.com>
2024-09-17 14:55:21 +0530

    Update dl.py
    Added: plot legend, activation function, ANN architecture

--- dl.py
+++ dl.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plot
+import math
 
+e=math.e
 #part b1:
 
 x=np.linspace(-2*np.pi, 2*np.pi,1000)
@@ -17,6 +19,19 @@ x_validation = np.random.uniform(-2*np.pi,2*np.pi,300)
 #ANN TO GENERATE Y-VALS
 
 #part b3:
-plot.plot(x,y)
+plot.plot(x,y, label='Sine Wave')
+plot.legend()
 
+#defining the activation functions and it's derivative
+def tanh(x):
+    return (e**x-e**(-x))/(e**x+e**(-x))
+def ddxtanhx(x):
+    return (1/math.cosh(x)**2)
 
+class NeuralNetwork:
+    #initialize weights
+    def __init__(self, input_size, h1_size, output_size):
+        self.weights_hidden_input=np.random.randn(input_size, hidden_size)
+        self.weights_hidden_output=np.random.randn(output_size, hidden_size)
+        self.bias_hidden=np.random.randn(input_size, hidden_size)
+        seld.bias_output=np.random.randn(output_size, hidden_size)
