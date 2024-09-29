3dd1072032b3f363c52817e567aa5d0a52ffa50e
SE21UCSE111 <160005432+SE21UCSE111@users.noreply.github.com>
2024-09-29 12:24:17 +0530

    Update Toy_problem.py

--- Toy_problem.py
+++ Toy_problem.py
@@ -86,8 +86,8 @@ class NeuralNetwork:
             loss = np.mean(np.square(y - full_output))
             print(f'Loss at epoch {epoch}: {loss}')
 
-            if epoch % 100 == 0:
-                plot.scatter(range(len(x)), y, label='True Data', alpha=0.6)
+            if epoch % 10 == 0:
+                plot.scatter(range(len(x)), y, label='True Data')
                 plot.plot(range(len(x)), full_output, label=f'Approximation at epoch {epoch}', color='red')
                 if not os.path.exists('plots'):
                     os.makedirs('plots')
